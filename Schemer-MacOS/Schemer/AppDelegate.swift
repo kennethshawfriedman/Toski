0e76521c0d326d7212ff2f83e2520930ff05fb0f
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-05-03 00:03:31 -0400

    Added MacOS GUI

---
+++ Schemer-MacOS/Schemer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Schemer
+//
+//  Created by Kenneth Friedman on 5/2/17.
+//  Copyright © 2017 Kenneth Friedman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Schemer-MacOS/Schemer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Schemer
+//
+//  Created by Kenneth Friedman on 5/2/17.
+//  Copyright © 2017 Kenneth Friedman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

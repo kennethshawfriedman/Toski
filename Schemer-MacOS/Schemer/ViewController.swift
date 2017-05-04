c21e1bb0ded8a0307a7030b3b5f2e512fa344f62
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-05-04 04:57:51 -0400

    Menus Return, Spell Check Turns Off, Modified Keys Record Themselves

--- Schemer-MacOS/Schemer/ViewController.swift
+++ Schemer-MacOS/Schemer/ViewController.swift
@@ -18,8 +18,12 @@ class ViewController: NSViewController, NSTextViewDelegate, NSTextStorageDelegat
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		//sets font to sourcecodepro and sets
+		//set up coding field #todo [these eventually should become part of the class CodeField class]
 		cf.font = NSFont(descriptor: NSFontDescriptor.init(name: "SourceCodePro-Regular", size: 16) , size: 16)
+		cf.isContinuousSpellCheckingEnabled = false
+		cf.isAutomaticSpellingCorrectionEnabled = false
+		cf.toggleContinuousSpellChecking(nil) //bizzare, but needed to prevent spell checking
+		
 		//Setting Delegates
 		cf.delegate = self
 		cf.textStorage?.delegate = self
@@ -30,27 +34,34 @@ class ViewController: NSViewController, NSTextViewDelegate, NSTextStorageDelegat
 			return $0
 		}
 		
-		//watch for flag changed
-		NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
-			self.flagsChanged(with: $0)
-			return $0
-		}
+		//watch for flag changed [we don't need this now because the keydown can check what modifiers are working
+//		NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+//			self.flagsChanged(with: $0)
+//			return $0
+//		}
 		
-		
-
 	}
 	
-//	override func keyDown(with event: NSEvent) {
-//	
-//	}
-	
+	//called on every key-stroke of non-modifier keys
 	override func keyDown(with event: NSEvent) {
+		super.keyDown(with: event)
 		print("wwworking bro")
+		print("KEY PRESSED: \(event.keyCode)")
+		print("MODS: \(event.modifierFlags)")
 	}
 	
-	override func flagsChanged(with event: NSEvent) {
-		print("ok now")
-	}
+	//called on every modifier change, no longer need this since keyDown will be able to check for modifiers
+//	override func flagsChanged(with event: NSEvent) {
+//		
+//		print(event.modifierFlags)
+//		
+//		if (event.modifierFlags.contains([.command])) {
+//			//print(event.characters ?? "shit")
+////			if (event.characters == "\n") {
+////				print("command enter!")
+////			}
+//		}
+//	}
 	
 }
 
@@ -66,8 +77,7 @@ extension ViewController {
 	}
 	
 	func textView(_ textView: NSTextView, shouldChangeTextInRanges affectedRanges: [NSValue], replacementStrings: [String]?) -> Bool {
-		Swift.print("sign of life")
-		print(replacementStrings)
+		print(replacementStrings ?? "rut roh!")
 		return true
 	}
 }

c7516c2b6a86eae1ddbed28876fe4c9190a0229a
kennethshawfriedman <kennethshawfriedman@gmail.com>
2017-05-04 03:30:25 -0400

    Can Get a Response From the NSTextField Now

--- Schemer-MacOS/Schemer/ViewController.swift
+++ Schemer-MacOS/Schemer/ViewController.swift
@@ -8,32 +8,50 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextViewDelegate, NSTextStorageDelegate {
 
+	let cfOLD = CodeField()
 	
-	@IBOutlet var scrollView: NSScrollView!
 	@IBOutlet var cf: CodeField!
+	@IBOutlet var scrollView: NSScrollView!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+
+		//sets font to sourcecodepro and sets
+		cf.font = NSFont(descriptor: NSFontDescriptor.init(name: "SourceCodePro-Regular", size: 16) , size: 16)
 		
+		//[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(controlTextDidChange:) name:NSControlTextDidChangeNotification object:nil];
+		//NSUserNotificationCenter.default.addObserver(cf, forKeyPath: <#T##String#>, options: <#T##NSKeyValueObservingOptions#>, context: <#T##UnsafeMutableRawPointer?#>)
 		
+		cf.delegate = self
+		
+		cf.textStorage?.delegate = self
 		
 		
 	}
 
 	override var representedObject: Any? {
 		didSet {
-		// Update the view, if already loaded.
+			
 		}
 	}
-
-
 }
 
+class CodeField : NSTextView {
+	
+	
+}
 
-class CodeField : NSTextField {
+extension ViewController {
 	
+	func textViewDidChangeSelection(_ notification: Notification) {
+		Swift.print("at least it's something")
+	}
 	
-	
+	func textView(_ textView: NSTextView, shouldChangeTextInRanges affectedRanges: [NSValue], replacementStrings: [String]?) -> Bool {
+		Swift.print("sign of life")
+		print(replacementStrings)
+		return true
+	}
 }
